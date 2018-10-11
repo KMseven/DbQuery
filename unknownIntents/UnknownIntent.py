2d0988ef76c673f0f8f00ff74c666e0561f7ac9a
Sudhanshu Kumar <sudhanshu.kumar@phenompeople.com>
2018-10-11 20:37:20 +0530

    seperating out unknown intent

--- unknownIntents/UnknownIntent.py
+++ unknownIntents/UnknownIntent.py
@@ -1,6 +1,29 @@
-class UnknownIntent():
-    def getUidForUnknownIntent(self):
-        print("don't do jaffa")
+from botScore.mongoQuery import Mongo
 
+class UnknownIntent():
+
+    def __init__(self,host):
+        self.host = host
+
+    def getUidForUnknownIntent(self,db,collection):
+        mongo = Mongo(self.host)
+        self.collection = mongo.get_db(db,collection)
+        docs = self.collection.find({"user_details.botAction":"UTTER_UNINTERPRETABLE"}, {"user_details.ptSessionId":1, "_id":0})
+        array = list(docs)
+        return array
+
+    def getDocForUid(self,ptSessionQuery):
+        ptSessionIdValue = ptSessionQuery['user_details']['ptSessionId']
+        docs = self.collection.find({"user_details.ptSessionId":ptSessionIdValue},{"user_details.request":1,"user_details.botResponse.messages":1,"_id":0})
+        array = list(docs)
+        return array
 if __name__==  "__main__":
-    print("caller")
+    obj = UnknownIntent("mongodb://pheglodev:goodDevelopers%401@dev-ng-mongo1.phenompeople.com:27017,dev-ng-mongo2.phenompeople.com:27017,dev-ng-mongo3.phenompeople.com:27017/mongo_ngcc_dev?readPreference=primary");
+    array = obj.getUidForUnknownIntent("mongo_chatbot","USER_CONVERSATIONS")
+    i = 0;
+    while i < len(array) :
+        print("id...",array[i])
+        arrayObj = obj.getDocForUid(array[i])
+        print("result = ",arrayObj)
+        i += 1
+    print("caller  ends")
